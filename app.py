d402bddda55f5df3a9318f968966bd4206558880
Bashirudeen Opeyemi Ibrahim <67709237+boiBASH@users.noreply.github.com>
2025-06-19 16:14:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1329,7 +1329,8 @@ tools = [
 
 # Initialize LiteLLMModel with the provided API key
 model = LiteLLMModel(
-    model_id="openrouter/openai/gpt-4o-2024-11-20",
+    #model_id="openrouter/openai/gpt-4o-2024-11-20",
+    model_id="openrouter/meta-llama/llama-4-maverick:free,
     temperature=0.3,
     api_key=api_key,
     additional_kwargs={
